d7648785ad339280efad7894bcbce4101f872577
fabioherberth <fabioherberth@hotmail.com>
2023-10-06 15:54:29 -0300

    Subindo o projeto para testar no render.

---
+++ placar/src/main/java/br/com/fk/placar/PlacarApplication.java
@@ -0,0 +1,13 @@
+package br.com.fk.placar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacarApplication.class, args);
+	}
+
+}
---
+++ placar/src/test/java/br/com/fk/placar/PlacarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fk.placar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
